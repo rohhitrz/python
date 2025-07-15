abbc34551a5bb3c332dc61582b2c58c2b4c9a318
Rohit <rohhit.rz@gmail.com>
2025-07-16 01:34:47 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,20 @@
+# multiline string
+
+# s1='''
+# hello what ra too much hen you are showing
+# '''
+
+# print(str)
+
+#strings are immutable
+
+# s2="thisisastring"
+# print(s2[-1])
+
+
+#String Immutability
+
+s3="hello";
+# s3[0]="G"  trying to change a character will create error
+s3='g'+s3[1:]   #instead create a new String
+print(s3)
